e1b3513539f641b046402f3bbea7a21c74d8c2ca
ta1kt0me <p.wadachi@gmail.com>
2015-03-15 14:28:49 +0900

    1:refactor:remove case

--- 1/movie.rb
+++ 1/movie.rb
@@ -4,15 +4,7 @@ class Movie
   CHILDRENS = 2
 
   attr_reader :title, :price_code
-
-  def price_code=(value)
-    @price_code = value
-    @price = case price_code
-      when REGULAR: RegularPrice.new
-      when NEW_RELEASE: NewReleasePrice.new
-      when CHILDRENS: ChildrensPrice.new
-    end
-  end
+  attr_writer :price
 
   def initialize(title, the_price_code)
     @title, self.price_code = title, the_price_code
