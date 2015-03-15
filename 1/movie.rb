4de2f44f9f4222d3b4f9d59d04bbc5e77e70c50d
ta1kt0me <p.wadachi@gmail.com>
2015-03-15 14:06:05 +0900

    1:refactor:apply state pattern first step

--- 1/movie.rb
+++ 1/movie.rb
@@ -3,11 +3,19 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+    @price = case price_code
+      when REGULAR: RegularPrice.new
+      when NEW_RELEASE: NewReleasePrice.new
+      when CHILDRENS: ChildrensPrice.new
+    end
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
 
   def charge(days_rented)
@@ -30,4 +38,16 @@ class Movie
   def frequent_renter_points(days_rented)
     (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
   end
+end
+
+class RegularPrice
+
+end
+
+class NewReleasePrice
+
+end
+
+class ChildrensPrice
+
 end
